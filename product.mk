0563368e4d51b800e062cfed88ff451d3a0ba5be
basamaryan <basam.aryan@gmail.com>
2023-05-22 11:02:41 +0000

    extra: Cleanup for my use
    Change-Id: I6a9e0e76fbf3893af7ec576fa3e01ff2c6cb9764

--- product.mk
+++ product.mk
@@ -1,16 +1,8 @@
-# Audio
-TARGET_EXCLUDES_AUDIOFX := true
-
 # Fonts
 PRODUCT_COPY_FILES += \
     vendor/extra/prebuilt/common/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
     vendor/extra/prebuilt/common/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
     vendor/extra/prebuilt/common/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf
-
-# Google Apps
-ifneq ($(BUILD_VANILLA), true)
-WITH_GMS := true
-$(call inherit-product, vendor/gms/products/gms.mk)
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
@@ -20,7 +12,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.kb_pad_port_r=4 \
     ro.com.google.ime.kb_pad_land_l=64 \
     ro.com.google.ime.kb_pad_land_r=64
-endif
 
 # I hate Safety Net
 ifneq ("$(wildcard  vendor/extra/prebuilt/etc/ih8sn_$(subst lineage_,,$(TARGET_PRODUCT)).conf)","")
@@ -34,7 +25,6 @@ endif
 # Overlay
 PRODUCT_PACKAGES += \
     ExtraFrameworksResTarget \
-    ExtraLineageSDKOverlay \
     ExtraSettingsResTarget
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/extra/overlay
